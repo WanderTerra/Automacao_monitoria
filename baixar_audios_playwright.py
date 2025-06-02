e7779b474c27c5d579fc9701433ceb3df3df2fa4
WanderTerra <105078842+WanderTerra@users.noreply.github.com>
2025-06-02 14:39:13 -0400

    Update baixar_audios_playwright.py

--- baixar_audios_playwright.py
+++ baixar_audios_playwright.py
@@ -36,7 +36,7 @@ FROM vonix.calls AS c
 WHERE queue_id LIKE 'aguas%'
     AND queue_id NOT LIKE 'aguasguariroba%'
     AND status LIKE 'Completada%'
-    AND start_time >= '2025-05-24 00:00:00'
+    AND start_time >= '2025-05-25 00:00:00'
     AND call_secs > 60
 ORDER BY start_time DESC
 ''',
@@ -49,7 +49,7 @@ SELECT call_id, queue_id, start_time, answer_time, hangup_time, call_secs
 FROM vonix.calls AS c
 WHERE queue_id LIKE 'vuon%'
     AND status LIKE 'Completada%'
-    AND start_time >= '2025-05-24 00:00:00'
+    AND start_time >= '2025-05-25 00:00:00'
     AND call_secs > 60
 ORDER BY start_time DESC
 ''',
